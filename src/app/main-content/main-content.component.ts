3d2fc4c5b9f24a90d0f9148ed6ee39720beec4d5
Dheymer León <dheymer@gmail.com>
2018-12-15 14:58:20 -0500

    Added functionalities Added playAll functionality Added playClip functionality (needs improvement) Added addClip functionality Added removeClip functionality Added input styling Fixed layout for playlist and clipManager sections

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -6,19 +6,18 @@ import { VideoRepositoryService } from '../video-repository.service';
   selector: 'app-main-content',
   templateUrl: './main-content.component.html'
 })
+
 export class MainContentComponent implements OnInit {
-  @Input('fullView') fullView: boolean;
-  @Input('fullVideo') fullVideo: string;
-  @Input('videoType') videoType: string;
-  videoDomContainer: any;
-  videoDomElement: any;
+  videoDomContainer: HTMLVideoElement;
   objVideo: Video;
   clips: Clip[] = [];
   filteredClips: Clip[] = [];
   selectedClip: Clip;
+  newClip: Clip = new Clip();
   selectedClipIndex: number;
   editClip: false;
   editVideo: false;
+  searchTag: string;
 
   constructor(
     private videoService: VideoRepositoryService
@@ -28,15 +27,17 @@ export class MainContentComponent implements OnInit {
   }
 
   ngOnInit() {
+    // tslint:disable-next-line:prefer-const
+    let self = this;
     this.videoDomContainer = document.querySelector('#videoPlayer');
-    this.videoDomElement = document.querySelector('#videoPlayerSource');
-    console.log(this.videoDomElement);
-    this.objVideo = new Video(
-      this.videoDomElement.src.split('#')[0],
-      this.videoDomElement.duration,
-      Math.round(this.videoDomElement.duration)
-    );
-    console.log(this.objVideo);
+    this.videoDomContainer.addEventListener('loadedmetadata', function() {
+      self.objVideo = new Video(
+        self.videoDomContainer.currentSrc.split('#')[0],
+        self.videoDomContainer.duration,
+        Math.round(self.videoDomContainer.duration)
+      );
+    });
+    console.log(this.newClip);
   }
 
   playClip(clip: Clip, clipIndex: number) {
@@ -44,12 +45,42 @@ export class MainContentComponent implements OnInit {
     this.selectedClip = clip;
     if (clip.startTime !== -1 && clip.endTime !== -1) {
       const clipSection = '#t' + clip.startTime + ',' + clip.endTime;
-      this.videoDomContainer.currentSrc = this.objVideo.videoUrl + clipSection;
+      this.videoDomContainer.src = this.objVideo.videoUrl + clipSection;
     } else {
-      this.videoDomContainer.currentSrc = this.objVideo.videoUrl;
+      this.videoDomContainer.src = this.objVideo.videoUrl;
     }
     this.videoDomContainer.load();
     this.videoDomContainer.play();
+  }
+
+  playClipTimeout(clip: Clip, clipIndex: number) {
+    return window.setTimeout(this.playClip(clip, clipIndex), 3000);
+  }
+
+  playAllClips() {
+    // tslint:disable-next-line:prefer-const
+    let self = this;
+    const clipCount = this.filteredClips.length - 1;
+    let i = 0;
+    let currentClip = this.filteredClips[0];
+
+    this.videoDomContainer.addEventListener('pause', function() {
+      if (i < clipCount) {
+        self.selectedClip.clipName = 'Timeout 3s';
+        currentClip = self.filteredClips[++i];
+        self.playClipTimeout(currentClip, i);
+      }
+    }, false);
+    this.playClip(currentClip, i);
+  }
+
+  removeClip(clipIndex: number) {
+    this.videoService.deleteClip(clipIndex);
+  }
+
+  addClip(clip: Clip) {
+    console.log(clip);
+    this.videoService.addClip(clip);
   }
 
 }
