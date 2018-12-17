a00c9d81f7519467831efbf53918a64df8fafe0a
Dheymer León <dheymer@gmail.com>
2018-12-17 14:01:13 -0500

    Code cleaning and documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+/**
+ * The app component
+ *
+ * @export
+ * @class AppComponent
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  fullView = true;
+  fullView = true;  // Flag that indicates if it's the Full mode (full functionality) or Demo mode (only play, no add, edit or delete)
   title = 'app';
+
+  /**
+   * Obtain the mode flag from the navbar
+   *
+   * @param {*} event The object from the emitter
+   * @memberof AppComponent
+   */
+  toggleMode(event) {
+    this.fullView = event.mode;
+  }
 }
